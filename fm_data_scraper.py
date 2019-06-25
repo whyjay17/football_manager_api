c78fff2bc2f78349cf9706b8e68a202f29523087
unknown <ykim164@illinois.edu>
2019-06-25 00:53:40 -0700

    Update fm_data_scraper.py: method-ize

--- fm_data_scraper.py
+++ fm_data_scraper.py
@@ -1,22 +1,21 @@
 import numpy as np
-# from nltk.corpus import stopwords
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://fmdataba.com/19/p/1165/lionel-messi/'
+def scrape_player_info(url):
+    r = requests.get(url, headers = {'User-Agent':'Mozilla/5.0'})
+    soup = bs(r.content, 'html.parser')
+    abilities = ['TECHNICAL', 'GOALKEEPING', 'MENTAL', 'PHYSICAL']
 
-r = requests.get(url, headers = {'User-Agent':'Mozilla/5.0'})
-soup = bs(r.content, 'lxml')
-abilities = ['TECHNICAL', 'MENTAL' , 'PHYSICAL']
+    def get_abilities(soup, keyword):
+        table = soup.select_one('div:has(h3:contains("' + keyword + '")) ~ div > table')
+        if table:
+            d = {item.select_one('td:nth-child(odd)').text:item.select_one('td:nth-child(even)').text for item in table.select('tr')}
+            return d
 
-def get_abilities(soup, keyword):
-    table = soup.select_one('div:has(h3:contains("' + ability + '")) + div > table')
-    d = {item.select_one('td:nth-child(odd)').text: int(item.select_one('td:nth-child(even)').text) for item in table.select('tr')}
-    return d
-
-results = {}
-
-for ability in abilities:
-    results[ability] = get_abilities(soup, ability)
-
-print(results)
+    results = {}
+    for ability in abilities:
+        stats = get_abilities(soup, ability)
+        results[ability] = stats if stats else {}
+    
+    return results
