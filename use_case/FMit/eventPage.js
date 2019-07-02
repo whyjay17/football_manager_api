35fe7e3852f890ba391bf7596874eb4d539ba917
unknown <ykim164@illinois.edu>
2019-07-02 00:48:15 -0700

    Add Chrome Extension use case of the API

---
+++ use_case/FMit/eventPage.js
@@ -0,0 +1,52 @@
+var menuItem = {
+    "id": "FMit",
+    "title": "FMit",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+const fixedEncodeURI = (str) => {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    let header = new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+    });
+
+    let myInit = {
+        method: 'GET',
+        headers: header,
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    url = 'http://localhost:5000/api/v1/players/' + fixedEncodeURI(clickData.selectionText)
+    return new Promise((reslove, reject) => {
+        fetch(url, myInit)
+            .then(response => response.json())
+            .then(responseText => {
+                var popup_url = 'popup.html'
+                var createData = {
+                    "url": popup_url,
+                    "type": "popup",
+                    "top": 5,
+                    "left": 5,
+                    "width": 570,
+                    "height": 535
+                };
+                chrome.windows.create(createData, function () {
+
+                });
+                // Store user data into a temp storage
+                console.log(responseText.result[0])
+                chrome.storage.sync.set({ 'player_info': responseText.result[0] }, function () { });
+            }).catch(err => {
+                reject(err);
+            });
+    }).catch(err => {
+        console.log(err);
+    });
+});
---
+++ use_case/FMit/popup.js
@@ -0,0 +1,37 @@
+// function to get strength and weakness
+
+const summarize_abiltiy = (abilities) => {
+    const sortedValues = Object.values(abilities).flatMap(Object.entries).sort(([, a], [, b]) => b - a);
+    const fiveHighest = sortedValues.slice(0, 5);
+    delete abilities.technical['Long Throws']
+    delete abilities.technical['Free Kick']
+    delete abilities.technical['Penalty Taking']
+    delete abilities.technical['Corners']
+    delete abilities.mental['Leadership']
+    const sortedValues2 = Object.values(abilities).flatMap(Object.entries).sort(([, a], [, b]) => b - a);
+    const fiveLowest = sortedValues2.slice(-5);
+
+    return { 'strength': fiveHighest, 'weakness': fiveLowest }
+}
+
+// Retrieve player data and display
+chrome.storage.sync.get(['player_info'], (data) => {
+    document.getElementById("player_age").innerHTML = data.player_info.age;
+    document.getElementById("player_foot").innerHTML = data.player_info.foot;
+    document.getElementById("player_nation").innerHTML = data.player_info.nationality;
+    document.getElementById("player_name").innerHTML = data.player_info.name;
+    document.getElementById("player_img").src = data.player_info.profile_img;
+    document.getElementById("player_pos").innerHTML = data.player_info.position;
+    let abilities = data.player_info.abilities
+    let summary = summarize_abiltiy(abilities)
+    console.log(summary)
+
+    for (let i = 0; i < 5; i++) {
+        document.getElementById("strength").innerHTML +=
+            "<span class=\"label label-success\">" + summary.strength[i][0] + ": " + summary.strength[i][1] + "</span>";
+        document.getElementById("weakness").innerHTML +=
+            "<span class=\"label label-danger\">" + summary.weakness[i][0] + ": " + summary.weakness[i][1] + "</span>";
+    }
+
+
+});
